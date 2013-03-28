48bb480a52eaf3966bd58ccf7ecc4be2337ce7db
Phil <philipstarner@hotmail.com>
2013-03-28 14:21:12 -0700

    latest full build out.
    Still need to fix canvas/replay button on lesson 1.

---
+++ Mars_Scaffold2/www/badge-it-gadget-lite/process-badges/test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Badge-It Gadget Lite Badger</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Badge-It Gadget Lite Badger">
+
+		<link rel="stylesheet" href="../css/style.css">
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.5.3/modernizr.min.js type="text/javascript"></script>
+
+
+  </head>
+
+  <body>
+		
+		You're email is: <?php echo $_POST["email"]; ?>
+	
+	</body>
+	
+</head>
